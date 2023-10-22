e82da8f002938b2fc2f8643bcdd53718865b4ddf
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-10-22 15:59:30 -0300

    Iniciando Projeto

---
+++ lab-springboot-api/src/test/java/labpadroesspring/LabPadroesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package labpadroesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
